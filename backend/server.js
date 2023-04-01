44768030e485c680eef0bfbfc884b3a115bbf8ef
Chris MacArthur <chriscsmacarthur@gmail.com>
2023-04-01 08:57:00 -0300

    Expanded architecture, reformatted comments

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,44 @@
-require("dotenv").config()
+require("dotenv").config() // Loads environment variables.
+
+/**
+ * IMPORTS
+ */
 const express = require("express") 
+path = require("path")
 const app = express()
 const http = require("http")
 const https = require("https")
 const fs = require("fs")
 
-app.get("/", (req, res) => { 
-    let message = "hi buddy "
+/**
+ * ENDPOINTS
+ */
+
+// Connects path to frontend (where index.html will be found and served by default).
+app.use(express.static(path.join(__dirname, "..", "frontend")));
+
+// Sample endpoint TBD.
+app.get("/hi", (req, res) => { 
+    let message = "hi buddy :)"
     //message += req.query.name
     res.send(message)
 })
 
-//--------------------------------------//
-//        STARTING THE SERVER           //
-//--------------------------------------//
 
-// Create either a HTTPS or HTTP server
+// For unrecognized GET requests, serve 404.html
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "..", "frontend", "404.html"));
+});
+
+
+
+/**
+ * INITIATION
+ */
+
+// Initiates the server, HTTP for PROD, HTTPS for DEV.
 let server, port;
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
 	const credentials = { // SSL certificate
 		key: fs.readFileSync(process.env.SSL_PRIVATE_KEY),
 		cert: fs.readFileSync(process.env.SSL_CERT)
@@ -29,7 +50,7 @@ if (process.env.NODE_ENV === 'production') {
 	port = process.env.PORT_DEV;
 }
 
-// Listen on a specific port.
+// Listen on the preferred port, depending on PROD or DEV.
 server.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
