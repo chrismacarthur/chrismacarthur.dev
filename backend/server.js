8f85838574a97670b711734ef69dc22792473d68
Chris MacArthur <chriscsmacarthur@gmail.com>
2023-03-31 21:37:31 -0300

    added code for running on prod

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
+require("dotenv").config()
 const express = require("express") 
 const app = express()
+const http = require("http")
+const https = require("https")
+const fs = require("fs")
 
 app.get("/", (req, res) => { 
     let message = "hi buddy "
@@ -7,6 +11,25 @@ app.get("/", (req, res) => {
     res.send(message)
 })
 
-app.listen(3000, () => {
-    console.log("App is now running on port 3000")
-})
+//--------------------------------------//
+//        STARTING THE SERVER           //
+//--------------------------------------//
+
+// Create either a HTTPS or HTTP server
+let server, port;
+if (process.env.NODE_ENV === 'production') {
+	const credentials = { // SSL certificate
+		key: fs.readFileSync(process.env.SSL_PRIVATE_KEY),
+		cert: fs.readFileSync(process.env.SSL_CERT)
+	}
+	server = https.createServer(credentials, app);
+	port = process.env.PORT_PROD;
+} else {
+	server = http.createServer(app);
+	port = process.env.PORT_DEV;
+}
+
+// Listen on a specific port.
+server.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+});
