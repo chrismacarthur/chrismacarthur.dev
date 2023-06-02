d94a1ab9fc9e3c237ce8d2d935567521913c49eb
Chris MacArthur <chriscsmacarthur@gmail.com>
2023-06-02 15:16:18 -0300

    Added working essay section, major CSS changes

---
+++ frontend/jsx/essay1.js
@@ -0,0 +1,53 @@
+const Essay1 = () => {
+  return (
+    <div className="essay-content">
+      <div className="essay-title">
+        The Expansion of Expression<br/><br/>
+      </div>
+      <div className="essay-body">
+        &nbsp;&nbsp;&nbsp;&nbsp;The common notion that the English language is in decline or “in a bad way” is often associated with those who hold a mindset viewed as stubborn (Orwell). George Orwell’s critiques come far before the introduction of the World Wide Web, which is undeniably one of the largest sources of creative expression in language today, but his literary forecast is still believed to be a relevant commentary.<sup>1</sup> Civilization as a whole changes with the development of societal values, so the art of language is sure to follow suit. Although English is not collapsing and Orwell’s sentiments are incorrect, it is clear that both the written and spoken forms of the language have mutated. In Orwell’s time, those who could not write eloquently likely had little use for writing; our world today has bestowed a public, online space to anyone who wishes to voice their opinion, as long as they have access to the Internet. Additionally, considering the context in which the supposedly devolved language sits is an important aspect to identifying the root of the issue at-hand. Analyzing the language’s usage and communicative value in our everyday lives can provide insight as to what is happening to the language. We live in an age where information is readily available, with online databases no further away than the nerve endings in our fingers. Due to the vast context shift of our language, it should not only be expected but wished-for that English twists to fit our newfound requirements.<br/><br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;Whether one agrees with Orwell’s criticism or not, it can be agreed that more people than ever have the privilege of a potential audience at their disposal through the Internet. With this virtual platform being so widespread in nature, the risk of becoming oversaturated greatly increases, as is supported by the growth of the Web.<sup>2</sup> This excess of output leads to a tsunami of content that can be troublesome to traverse, and may be the primary pillar that upholds the thought of crippled English. Public writing has become increasingly normalized in our culture, so it is natural that people feel less of an obligation to maintain formality in their writing. It is important to note that underneath the dull abbreviations and overused phrasing, the Internet hosts a thriving ecosystem of original thought. Film blogs, music articles, and philosophical write-ups are among the flourishing virtual habitats that are rich with inspiration, catalyzed by the networking allowance of cyberspace; these essential spaces for the written form should not be wrongfully desecrated by those who attempt to summarize an entire language from a surface view.<br/><br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;With this in mind, we should be asking ourselves <i>how</i> the language is used before questioning how well it is used. If a theoretical, scholastic image of the world during Orwell’s life can be imagined, a picture of lower education and literacy rates may partially paint a person’s mental frame. Although the percentage of active readers and writers has been on a general incline since the 20th century, it would be ignorant to believe that higher literacy rates correlate to an absolute elevated usage of the language, at least not directly.<sup>3</sup> Even so, the previously mentioned expansion of the written form does indicate that a context change is occurring, and context is important. Writing was previously reserved for a formal backdrop. If someone were to sit down and compose a physical letter, time would be taken to ensure eloquence and clarity. Today, society moves at such a rapid pace that we are not required to nitpick over such details in a casual setting. Without reducing this essay to an advert for social media, communication from afar has never been easier; one is able to have an entire written discussion in real time from anywhere in the world. English is filled with spoken colloquialisms that have found themselves injected into the written form of the language - now that it carries the same spontaneity and lack of professionalism in specific situations that the oral version does - but do not mistake this for a decline.<br/><br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;The previous points mentioned are primarily aimed at countering Orwell’s stance on language, but it is equally important to recognize the undebatable benefits English has undergone in the years since Orwell’s time. With online connections, people are exposed to different cultures and backgrounds, leading to a lower amount of localized versions of the language and an increase in standardization.<sup>4</sup> In recent years, many cultures have been making active efforts to become more inclusive, and a significant portion of this progress belongs to language. The adaptation of gender-neutral phrasing, preferred pronoun expression, and various other changes to how we address others is certainly a push in the right direction for a better communicative medium.<br/><br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;Although English has not degraded, the language is capable of being improved, as it always will be. It is challenging to juggle every variable in a language’s development, but several vital factors have created a strong argument against Orwell’s criticism. The introduction of the Internet is both a primary source for Orwell sympathizers and a blistering counterexample; it allows linguists to share and express themselves in new ways, although Orwell might have considered the Web to be a curse by judging informal, digital conversation. English has also undergone drastic context changes in how we use it and what is considered proper during interpersonal communication. The rapidly moving climate of the writing realm has permitted the language to become noticeably more inclusive and fast-paced. This acceleration does not lead to the destruction of conscious thought, but does result in less-than-thorough instances of exchange. Language is a construct that has never truly been perfected, unless a perfect thing is subject to change. Change can be frustrating and uncomfortable, but the growth our language has undergone should be embraced as it adapts to its new circumstances. As Peter Daniels exclaims, “Humankind is defined by language; but civilization is defined by writing” (Daniels 1). One day, we may hope to see our language perfected.
+
+        <hr className="divider" />
+
+        <div className="essay-notes">
+          <div className="note">
+            <sup>1</sup> See <i>Best Writing</i>'s'  “Writing Statistics: Facts, Salaries & Industry Trends”  article for evidence of the expansive online writing economy.<br/>
+          </div>
+          <div className="note">
+            <sup>2</sup> <i>Our World in Data</i> depicts the steady growth of the Internet and its users over the years.<br/>
+          </div>
+          <div className="note">
+            <sup>3</sup> The <i>National Center for Education Statistics</i> provides documentation on the rising literacy rates over time within the United States of America.<br/>
+          </div>
+          <div className="note">
+            <sup>4</sup> Yusuf Bhana details the Internet’s effects on the exchange between different versions of English in “How the Internet is Changing Language”. British and American versions of English are used as examples for the “seeding of conventions” (Bhana).<br/>
+          </div>
+        </div>
+
+        <hr className="divider" />
+
+        <div className="essay-citations-title">
+          Sources<br/>
+        </div>
+
+        <div className="essay-citations">
+          Orwell, George. “Politics and the English Language.” <i>The Orwell Foundation</i>, Originally Published 1946. https://www.orwellfoundation.com/the-orwell-foundation/orwell/essays-and-other-works/politics-and-the-english-language/. Accessed 11 January 2023.<br/><br/>
+          
+          Laurinavicius, Tomas. “Writing Statistics: Facts, Salaries & Industry Trends.” <i>Best Writing</i>. https://bestwriting.com/blog/writing-statistics. Accessed 19 January 2023.<br/><br/>
+          
+          “Adoption of communication technologies per 100 people.” <i>Our World in Data</i>. https://ourworldindata.org/grapher/ict-adoption-per-100-people. Accessed 18 January 2023.<br/><br/>
+          
+          “National Assessment of Adult Literacy.” <i>National Center for Education Statistics</i>. https://nces.ed.gov/naal/lit_history.asp. Accessed 19 January 2023.<br/><br/>
+          
+          Bhana, Yusuf . “How the Internet is Changing Language.” <i>Toppan Digital Language</i>. https://toppandigital.com/us/blog-us/how-the-internet-is-changing-language/. Accessed 20 January 2023.<br/><br/>
+          
+          Daniels, Peter. <i>The World’s Writing Systems</i>. Oxford, 1996.
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/jsx/site.js
+++ frontend/jsx/site.js
@@ -164,28 +164,145 @@ function Projects() {
 }
 
 
-{/* "Essays" portion of the webpage, TBA. */}
+{/* "Essays" portion of the webpage. */}
 function Essays() {
+
+  {/* Tracks essay selection and hover status. */}
+  const [selectedEssayIndex, setSelectedEssayIndex] = React.useState(null);
+  const [isEssayHovered, setIsEssayHovered] = React.useState(false);
+
+  {/* Tracks the current "view" setting (classic, wide). */}
+  const [currentView, toggleView] = React.useState(true);
+  const view = currentView ? "wide" : "classic";
+
+  {/* Tracks the current "mode" setting (dark, light). */}
+  const [currentMode, toggleMode] = React.useState(true);
+  const mode = currentMode ? "dark-mode" : "light-mode";
+
+  {/* Tracks the status of the hover animation "essayTransition" in essay.css. */}
+  const [isTransitioning, setTransitioning] = React.useState(false);
+
+  const handleEssayClick = (index) => {
+    setSelectedEssayIndex(index);
+  };
+
+  const handleEssayHover = () => {
+    setIsEssayHovered(true);
+  };
+
+  const handleEssayLeave = () => {
+    setIsEssayHovered(false);
+  };
+
+  const handleBack = () => {
+    setSelectedEssayIndex(null);
+  };
+
+  const handleMode = () => {
+    setTransitioning(true);
+    toggleMode(!currentMode);
+
+    setTimeout(() => {
+      setTransitioning(false);
+    }, 300);
+  };
+
+  const handleView = () => {
+    setTransitioning(true);
+    toggleView(!currentView);
+
+    setTimeout(() => {
+      setTransitioning(false);
+    }, 300);
+  };
+  
+  const handleTransitionEnd = () => {
+    setTransitioning(false);
+  };
+
+  {/* Indexing information for displaying previews. */}
+  const essays = [
+    {
+      title: "From Analog to Digital: Why the Album Is Here to Stay",
+      image_url: "./images/shelf.jpg",
+      image_height: "top",
+      component: Essay0,
+    },
+    {
+      title: "The Expansion of Expression",
+      image_url: "./images/books.jpg",
+      image_height: "top",
+      component: Essay1,
+    },
+  ];
+
   return (
     <section id="essays">
-      <h2>Essays</h2>
-      <div class="essay-container">
-        <div class="essay">
-          TBA
-          <div class="essay-description">
-            Coming Soon!
+
+      {/* Show page header if an essay isn't selected */}
+      {selectedEssayIndex === null ? (
+        <div className="essay-header">
+          <h2>Essays</h2>
+          <hr className="divider" />
+        </div>
+      ) : (
+        <div className="essay-options">
+          <div className="essay-options-left">
+            <button id="essay-return" onClick={handleBack}>Back</button>
+            <div class="essay-help">
+            <span class="help-icon">?</span>
+            <div class="help-text">
+              Use the options on the right side of the screen to change the viewing format.
+            </div>
+          </div>
+
+          </div>
+          <div className="essay-options-right">
+            <button id="essay-mode" onClick={handleMode}>Switch to {currentMode ? "Light Mode" : "Dark Mode"}</button>
+            <button id="essay-view" onClick={handleView}>Switch to {currentView ? "Classic View" : "Wide View"}</button>
           </div>
         </div>
-      </div>
+      )}
+
+      {selectedEssayIndex === null ? (
+        <div className="essay-list">
+          {essays.map((essay, index) => (
+            <div
+              id="essay-preview"
+              className={`text-content ${isEssayHovered && selectedEssayIndex !== index ? "hover" : ""}`}
+              style={{
+                backgroundImage: `url(${essay.image_url})`,
+                backgroundPosition: `${essay.image_height}`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat"
+              }}
+              key={index}
+              onClick={() => handleEssayClick(index)}
+              onMouseEnter={() => handleEssayHover(index)}
+              onMouseLeave={() => handleEssayLeave(index)}
+            >
+              <p>{essay.title}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div
+        className={`${mode} ${view} ${isTransitioning ? "essay-transition" : ""}`}
+        onTransitionEnd={handleTransitionEnd}
+        > 
+          {React.createElement(essays[selectedEssayIndex].component)}
+        </div>
+      )}
     </section>
   );
 }
+
 
 {/* Main function used to determine the <main> body of the SPA, depending on the active nav bar selection */}
 function Main(props) {
 
   React.useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "auto" });
   }, [props.activePage]);
 
   return (
