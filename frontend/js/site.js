012e97a1d69b821301da16e9f17f46eb6498b161
Chris MacArthur <chriscsmacarthur@gmail.com>
2023-06-06 19:14:23 -0300

    Separated Essay component and tweaked file struct

--- frontend/js/site.js
+++ frontend/js/site.js
@@ -181,130 +181,6 @@ function Projects() {
     id: "projects"
   }, /*#__PURE__*/React.createElement("h2", null, "Under Construction.."), /*#__PURE__*/React.createElement("p", null, "Eventually, this section will act as a hub for programming projects I've worked on. Stay posted!"));
 }
-{/* "Essays" portion of the webpage. */}
-function Essays() {
-  {/* Tracks essay selection and hover status. */}
-  var _React$useState = React.useState(null),
-    _React$useState2 = _slicedToArray(_React$useState, 2),
-    selectedEssayIndex = _React$useState2[0],
-    setSelectedEssayIndex = _React$useState2[1];
-  var _React$useState3 = React.useState(false),
-    _React$useState4 = _slicedToArray(_React$useState3, 2),
-    isEssayHovered = _React$useState4[0],
-    setIsEssayHovered = _React$useState4[1];
-  {/* Tracks the current "view" setting (classic, wide). */}
-  var _React$useState5 = React.useState(true),
-    _React$useState6 = _slicedToArray(_React$useState5, 2),
-    currentView = _React$useState6[0],
-    toggleView = _React$useState6[1];
-  var view = currentView ? "wide" : "classic";
-  {/* Tracks the current "mode" setting (dark, light). */}
-  var _React$useState7 = React.useState(true),
-    _React$useState8 = _slicedToArray(_React$useState7, 2),
-    currentMode = _React$useState8[0],
-    toggleMode = _React$useState8[1];
-  var mode = currentMode ? "dark-mode" : "light-mode";
-  {/* Tracks the status of the hover animation "essayTransition" in essay.css. */}
-  var _React$useState9 = React.useState(false),
-    _React$useState10 = _slicedToArray(_React$useState9, 2),
-    isTransitioning = _React$useState10[0],
-    setTransitioning = _React$useState10[1];
-  var handleEssayClick = function handleEssayClick(index) {
-    setSelectedEssayIndex(index);
-  };
-  var handleEssayHover = function handleEssayHover() {
-    setIsEssayHovered(true);
-  };
-  var handleEssayLeave = function handleEssayLeave() {
-    setIsEssayHovered(false);
-  };
-  var handleBack = function handleBack() {
-    setSelectedEssayIndex(null);
-  };
-  var handleMode = function handleMode() {
-    setTransitioning(true);
-    toggleMode(!currentMode);
-    setTimeout(function () {
-      setTransitioning(false);
-    }, 300);
-  };
-  var handleView = function handleView() {
-    setTransitioning(true);
-    toggleView(!currentView);
-    setTimeout(function () {
-      setTransitioning(false);
-    }, 300);
-  };
-  var handleTransitionEnd = function handleTransitionEnd() {
-    setTransitioning(false);
-  };
-  {/* Indexing information for displaying previews. */}
-  var essays = [{
-    title: "From Analog to Digital: Why the Album Is Here to Stay",
-    image_url: "./images/shelf.jpg",
-    image_height: "top",
-    component: Essay0
-  }, {
-    title: "The Expansion of Expression",
-    image_url: "./images/books.jpg",
-    image_height: "top",
-    component: Essay1
-  }];
-  return /*#__PURE__*/React.createElement("section", {
-    id: "essays"
-  }, selectedEssayIndex === null ? /*#__PURE__*/React.createElement("div", {
-    className: "essay-header"
-  }, /*#__PURE__*/React.createElement("h2", null, "Essays"), /*#__PURE__*/React.createElement("hr", {
-    className: "divider"
-  })) : /*#__PURE__*/React.createElement("div", {
-    className: "essay-options"
-  }, /*#__PURE__*/React.createElement("div", {
-    className: "essay-options-left"
-  }, /*#__PURE__*/React.createElement("button", {
-    id: "essay-return",
-    onClick: handleBack
-  }, "Back"), /*#__PURE__*/React.createElement("div", {
-    "class": "essay-help"
-  }, /*#__PURE__*/React.createElement("span", {
-    "class": "help-icon"
-  }, "?"), /*#__PURE__*/React.createElement("div", {
-    "class": "help-text"
-  }, "Use the options on the right side of the screen to change the viewing format."))), /*#__PURE__*/React.createElement("div", {
-    className: "essay-options-right"
-  }, /*#__PURE__*/React.createElement("button", {
-    id: "essay-mode",
-    onClick: handleMode
-  }, "Switch to ", currentMode ? "Light Mode" : "Dark Mode"), /*#__PURE__*/React.createElement("button", {
-    id: "essay-view",
-    onClick: handleView
-  }, "Switch to ", currentView ? "Classic View" : "Wide View"))), selectedEssayIndex === null ? /*#__PURE__*/React.createElement("div", {
-    className: "essay-list"
-  }, essays.map(function (essay, index) {
-    return /*#__PURE__*/React.createElement("div", {
-      id: "essay-preview",
-      className: "text-content ".concat(isEssayHovered && selectedEssayIndex !== index ? "hover" : ""),
-      style: {
-        backgroundImage: "url(".concat(essay.image_url, ")"),
-        backgroundPosition: "".concat(essay.image_height),
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat"
-      },
-      key: index,
-      onClick: function onClick() {
-        return handleEssayClick(index);
-      },
-      onMouseEnter: function onMouseEnter() {
-        return handleEssayHover(index);
-      },
-      onMouseLeave: function onMouseLeave() {
-        return handleEssayLeave(index);
-      }
-    }, /*#__PURE__*/React.createElement("p", null, essay.title));
-  })) : /*#__PURE__*/React.createElement("div", {
-    className: "".concat(mode, " ").concat(view, " ").concat(isTransitioning ? "essay-transition" : ""),
-    onTransitionEnd: handleTransitionEnd
-  }, React.createElement(essays[selectedEssayIndex].component)));
-}
 {/* Main function used to determine the <main> body of the SPA, depending on the active nav bar selection */}
 function Main(props) {
   React.useEffect(function () {
@@ -322,10 +198,10 @@ function Footer() {
 {/* The central function used to construct the active web components. Responds to props.activePage. */}
 function App() {
   {/* Sets the default <Main> content to the Intro function (loading from base URL "https://chrismacarthur.dev"). */}
-  var _React$useState11 = React.useState("intro"),
-    _React$useState12 = _slicedToArray(_React$useState11, 2),
-    activePage = _React$useState12[0],
-    setActivePage = _React$useState12[1];
+  var _React$useState = React.useState("intro"),
+    _React$useState2 = _slicedToArray(_React$useState, 2),
+    activePage = _React$useState2[0],
+    setActivePage = _React$useState2[1];
   {/* Checks the hash portion of the URL (#portfolio) to set the appropriate content when loading from section.
    "[]" as our second argument tells React to only mount this function once, as it is only required for an initial load. */}
   React.useEffect(function () {
