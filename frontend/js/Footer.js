e6d297b4327e2ad5b04269b242868f84b8004e71
Chris MacArthur <chriscsmacarthur@gmail.com>
2023-06-17 14:05:10 -0300

    Removed unnecessary build files

--- frontend/js/Footer.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-{/* Outlines a short footer, for styling purposes. */}
-function Footer() {
-  return /*#__PURE__*/React.createElement("footer", null);
-}
--- frontend/js/Projects.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-{/* "Projects" portion of the webpage, TBA. */}
-function Projects() {
-  return /*#__PURE__*/React.createElement("section", {
-    id: "projects"
-  }, /*#__PURE__*/React.createElement("h2", null, "Under Construction.."), /*#__PURE__*/React.createElement("p", null, "Eventually, this section will act as a hub for programming projects I've worked on. Stay posted!"));
-}
